c94923f4aea2eddf5276b5d41e0fa4fbd84083a3
Artemis <54802115+ikigaicode@users.noreply.github.com>
2022-09-02 19:25:05 -0700

    Create Section-3.sql
    Exercises of DROP

---
+++ Section-3.sql
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+--- 22. Dropping Databases: to delete databases on MySQL (or any MDB).
+
+DROP DATABASES <name>;
+
+Examples:
+
+1) DROP DATABASE hello_world_db;
+
+and then to check again the database modified, write the query:
+
+1-1) show databases;
+
+it should show a databases list.
+
+2) DROP DATABASE testing_db;
+and then to check again the database modified, write the query:
+2-1) show databases;
+it should show a databases list.
+
