250a1f66f1f31ad149231895c11586f8b70d0c0c
Artemis <54802115+ikigaicode@users.noreply.github.com>
2022-09-02 19:33:20 -0700

    Update Section-3.sql
    Commenting lines that are not sql code.

--- Section-3.sql
+++ Section-3.sql
@@ -9,20 +9,33 @@
 
 --- 22. Dropping Databases: to delete databases on MySQL (or any MDB).
 
+-- Exercises:
+
 DROP DATABASE <name>;
 
-Examples:
+-- Examples:
 
 1) DROP DATABASE hello_world_db;
 
-and then to check again the database modified, write the query:
+-- and then to check again the database modified, write the query:
 
 1-1) show databases;
 
-it should show a databases list.
+--it should show a databases list.
 
 2) DROP DATABASE testing_db;
-and then to check again the database modified, write the query:
+-- and then to check again the database modified, write the query:
 2-1) show databases;
-it should show a databases list.
+-- it should show a databases list.
 
+
+--- 23. -- CODE: Dropping Databases
+--To drop a database:
+
+DROP DATABASE database_name; 
+
+--For Example:
+
+DROP DATABASE hello_world_db; 
+
+-- Remember to be careful with this command! Once you drop a database, it's gone!
