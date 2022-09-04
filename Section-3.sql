d1ae6a23ffac02004d42ae9dea32416bbb867006
Artemis <54802115+ikigaicode@users.noreply.github.com>
2022-09-04 12:17:46 -0700

    Update Section-3.sql
    Continued studying on Sunday

--- Section-3.sql
+++ Section-3.sql
@@ -7,7 +7,7 @@
 
 -- ***********************************************************************************************************
 
---- 22. Dropping Databases: to delete databases on MySQL (or any MDB).
+--- ** 22. Dropping Databases: to delete databases on MySQL (or any MDB). **
 
 -- Exercises:
 
@@ -30,10 +30,11 @@ DROP DATABASE <name>;
 
 -- ***********************************************************************************************************
 
---- 23. -- CODE: Dropping Databases
+--- ** 23. CODE: Dropping Databases **
 --To drop a database:
 
 DROP DATABASE database_name; 
+-- use to delete a database.
 
 --For Example:
 
@@ -44,7 +45,33 @@ DROP DATABASE hello_world_db;
 
 -- ***********************************************************************************************************
 
--- 24. Using Databases
+-- ** 24. Using Databases **
 
 USE <database name>;
+-- Use command in SQL.
+-- And what this does is it tells my SQL which database we want to be working with.
+
+SELECT database();
+-- If we wanted to know for sure what database we are currently using, MySQL command SELECT is the one, SELECT database with parentheses after it.
+-- will tell you the currently used database.
+-- 
+
+-- Show NULL when we are not using a database in that instant. 
+
+-- Like, first:
+
+-- USE <database name>; 
+-- and then
+-- SELECT database();
+
+-- ** 25. CODE: Using Databases **
+
+USE <database name>;
+ -- example:
+            USE dog_walking_app;
+ 
+SELECT database();
+
+
+-- ** 26. Introduction to Tables ***
 
